dac2118b4330f2b78cb180ebc5a856e26c355375
KyleDicksonx <kyleo.dickson@gmail.com>
2025-05-15 12:41:53 -0700

    Ship and CruseShip done but w/o comments yet.

--- CS111Lab6/src/lab6/CruiseShip.java
+++ CS111Lab6/src/lab6/CruiseShip.java
@@ -1,5 +1,33 @@
 package lab6;
 
-public class CruiseShip {
-
+public class CruiseShip extends Ship{
+	protected int numPassengers;
+	
+	public CruiseShip(String name, int numEngines, int numPassengers) {
+		super(name, numEngines);
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "Name: " + getName() + 
+				"\nYear Built: " + getYearBuilt() + 
+				"\nNumber of Engines: " + getNumEngines() + 
+				"\nNumber of Passengers: " + this.numPassengers;
+	}
+	
+	
+	@Override
+	public String info() {
+		return "There are 1764 active cruse ships in the world.\n(Note: I just made that up)";
+	}
+	
+	
+	public int getNumPassengers() {
+		return this.numPassengers;
+	}
+	
+	public void setNumPassengers(int passengers) {
+		this.numPassengers = passengers;
+	}
 }
--- CS111Lab6/src/lab6/Ship.java
+++ CS111Lab6/src/lab6/Ship.java
@@ -1,5 +1,31 @@
 package lab6;
 
-public abstract class Ship {
-
+public abstract class Ship extends Vehicle{
+	
+	protected int numEngines;
+	
+	public Ship(String name, int numEngines) {
+		super(name);
+		this.numEngines = numEngines;
+	}
+	
+	
+	public String info() {
+		return "Ships float on water. \nMost of the time...";
+	}
+	
+	public int getNumEngines() {
+		return this.numEngines;
+	}
+	
+	public void setNumEngines(int numEngines) {
+		this.numEngines = numEngines;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
