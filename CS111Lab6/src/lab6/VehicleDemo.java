a66f67d7f183bfe5f921d7a2874012300709daa9
KyleDicksonx <kyleo.dickson@gmail.com>
2025-05-15 12:48:51 -0700

    VehicleDemo Started.

---
+++ CS111Lab6/src/lab6/VehicleDemo.java
@@ -0,0 +1,24 @@
+package lab6;
+
+public class VehicleDemo {
+	
+	Vehicle ship1 = new CruiseShip("Quaker Oats Express", 12, 11261);
+	Vehicle ship2 = new CruiseShip("The Raines Rowboat", 4, 1000);
+	Vehicle ship3 = new CruiseShip("The Land Rover", 7, 2364);
+	
+	Vehicle heli1 = new Helicopter("Totally a Tank", 17500.0, 457);
+	Vehicle heli2 = new Helicopter("Republic Gunship", 22256.0, 237);
+	Vehicle heli3 = new Helicopter("Star Desroyer");
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
